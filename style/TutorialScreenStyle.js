9ad1bf6e1c0ac8df431ffb1c8daf1aff8ca40f3f
OKJAEHO <113006936+OKHAEHO@users.noreply.github.com>
2024-03-14 19:37:39 +0900

    Feat : 튜토리얼 closed#5 (#7)

---
+++ style/TutorialScreenStyle.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const TutorialContainer = styled.View`
+  padding-top: 10px;
+  padding-left: 20px;
+  padding-right: 20px;
+  padding-bottom: 10px;
+`;
+
+export const TutorialRow = styled.View`
+  flex-direction: row;
+`;
