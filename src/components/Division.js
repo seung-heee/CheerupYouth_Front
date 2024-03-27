ed5346884e53f0e8cf1077ae7636d8777cb1590f
Kim.D.Jun <99592596+KayDiJay@users.noreply.github.com>
2024-03-28 02:39:25 +0900

    Feat: 로그인 UI closed #6 (#10)

---
+++ src/components/Division.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components/native';
+import * as S from "../../style/LoginStyle";
+
+
+const Division = () => {
+  return (
+    <S.DivisionContainer>
+      <S.Line />
+      <S.DivisionText>소셜 계정을 통해 로그인</S.DivisionText>
+    </S.DivisionContainer>
+  );
+};
+
+export default Division;
--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,11 +1,26 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import styled from 'styled-components/native';
+import Logo from "../components/Logo";
+import Id from "../components/Id";
+import Id_Pw_Sign from "../components/Id_Pw_Sign";
+import Division from "../components/Division";
+import { SafeAreaView, View,  } from 'react-native';
+import * as S from "../../style/LoginStyle";
 
 const LoginScreen = () => {
   return (
-    <View>
-      <Text>로그인</Text>
-    </View>
+    <SafeAreaView style={{ flex: 1, backgroundColor: '#fff' }}>
+      <S.MainContainer>
+        <S.LogoContainer>
+          <Logo />
+        </S.LogoContainer>
+        <S.InputContainer>
+          <Id />
+          <Id_Pw_Sign />
+        </S.InputContainer>
+        <Division />
+      </S.MainContainer>
+    </SafeAreaView>
   );
 };
 
