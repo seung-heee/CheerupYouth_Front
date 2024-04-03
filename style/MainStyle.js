ed1a7e789e3f8a04cb1a28b399c1babf31b62c13
jeongminiii <112999577+jeongminiii@users.noreply.github.com>
2024-04-04 01:32:05 +0900

    Fix : main 스타일 수정 (#11)

--- style/MainStyle.js
+++ style/MainStyle.js
@@ -4,21 +4,21 @@ export const Container = styled.View`
   flex: 1;
   background-color: #f2f3f8;
   padding-horizontal: 20px;
+  margin-top: 50px;
 `;
 
 export const Header = styled.View`
   align-items: center;
-  justify-content: flex-end;
+  justify-content: space-between;
   flex-direction: row;
-  margin-top: 10px; /* Adjust this value if the top bar is removed */
+  margin-top: 10px;
   margin-bottom: 10px;
 `;
 
 export const HeaderText = styled.Text`
   font-size: 20px;
   font-weight: bold;
-  align-items: center;
-  margin-right: 90px;
+  text-align: center;
 `;
 
 export const MenuText = styled.Text`
@@ -52,18 +52,13 @@ export const InnerText = styled.Text`
 `;
 
 export const Button = styled.TouchableOpacity`
-  shadow-color: #000000;
-  shadow-opacity: 0.27;
-  shadow-radius: 4.65;
-  shadow-offset: {
-    width: 0;
-    height: 3;
-  }
-  margin: 3px;
-`;
-
-export const ButtonText = styled.Text`
   background-color: white;
-  padding: 10px;
   margin: 5px;
+  border-radius: 10px;
+  padding: 10px;
+  shadow-color: #000;
+  shadow-offset: 0 2px;
+  shadow-opacity: 0.2;
+  shadow-radius: 2px;
+  elevation: 2; /* 안드로이드에서 그림자 효과 */
 `;
