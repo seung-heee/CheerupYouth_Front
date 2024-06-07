3dbadc28a3ef496fd1f14e5e5bb9e21a54e5bb45
seung-heee <tmdgmldi2595@naver.com>
2024-06-07 20:16:31 +0900

    fix: 정책세부내용 modal 수정

--- src/screens/policy/PolicyDetail_fin.js
+++ src/screens/policy/PolicyDetail_fin.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, TouchableOpacity, Image} from "react-native";
+import { View, Text, TouchableOpacity, Image, Modal, StyleSheet} from "react-native";
 import * as P from "../../../style/policy";
 import {useFocusEffect, useNavigation} from '@react-navigation/native';
 import axios from 'axios';
@@ -10,6 +10,13 @@ import { SERVER_URL } from "../../components/ServerAddress";
 const PolicyDetail_fin = ({route, navigation}) => {
   const { key } = route.params;
   const [ policy, setPolicy ] = useState({})
+  const [ isSupportedOpen, setIsSupportedOpen ] = useState(false);
+  const [ isExcludedOpen, setIsExcludedOpen ] = useState(false);
+  
+  const handlePress = () => {
+    console.log('아이템 클릭됨!');
+    // 추가적인 로직을 여기에 작성합니다.
+  };
 
   const getSelectPolicy = async () => {
     try {
@@ -68,13 +75,21 @@ const PolicyDetail_fin = ({route, navigation}) => {
 
         {policy.supported && (
           <P.contentBox>
-            <P.contentBoxTitle>지원대상</P.contentBoxTitle>
+            <View
+              style={{
+                flex: 1,
+                flexDirection: 'row', // 가로 배치
+                alignItems: 'center', // 세로 중앙 정렬
+              }} 
+            >
+              <P.contentBoxTitle>지원대상</P.contentBoxTitle>
+              <Icon onPress={() => setIsSupportedOpen(true)} name="wechat" size={25} color="#2E4B8F" />
+            </View>
             <P.contentBoxContent>{policy.content?.supported_target}</P.contentBoxContent>
             {Object.keys(policy.supported)
               .filter(key => key.startsWith('conditions'))
               .map(key => {
                 // if (!policy.supported[key]) return null;
-
                 return (
                 <P.contentBoxContent>
 
@@ -87,7 +102,16 @@ const PolicyDetail_fin = ({route, navigation}) => {
 
         {policy.excluded && (
           <P.contentBox>
-            <P.contentBoxTitle>제외대상</P.contentBoxTitle>
+            <View
+              style={{
+                flex: 1,
+                flexDirection: 'row', // 가로 배치
+                alignItems: 'center', // 세로 중앙 정렬
+              }} 
+            >
+              <P.contentBoxTitle>제외대상</P.contentBoxTitle>
+              <Icon onPress={() => setIsExcludedOpen(true)} name="wechat" size={25} color="#2E4B8F" />
+            </View>
             {Object.keys(policy.excluded)
               .filter(key => key.startsWith('conditions'))
               .map(key => {
@@ -129,8 +153,93 @@ const PolicyDetail_fin = ({route, navigation}) => {
           <P.contentBoxContent>{policy.content?.submission_papers}</P.contentBoxContent>
         </P.contentBox>}
       </ScrollView>
+
+      {/* 지원대상 모달창 */}
+      <View style={styles.container}>
+        <Modal
+          visible={isSupportedOpen}
+          onShow={() => console.log('지원대상 모달창')}
+          onRequestClose={() => setIsSupportedOpen(false)}
+          transparent={true} // 필요에 따라 true로 설정할 수 있습니다.
+          style={{ alignItems: 'center' }}
+          >
+          <TouchableOpacity style={styles.modalBackground} activeOpacity={1} onPressOut={() => setIsSupportedOpen(false)}>
+            <View style={styles.modalContainer}>
+              <Text style={styles.modalTitle}>지원대상 매칭</Text>
+              <Text style={styles.modalContent}>지원대상 매칭 내용</Text>
+              <TouchableOpacity style={styles.closeButton} onPress={() => setIsSupportedOpen(false)}>
+                <Text style={styles.closeButtonText}>X</Text>
+              </TouchableOpacity>
+            </View>
+          </TouchableOpacity>
+        </Modal>
+      </View>
+
+      {/* 제외대상 모달창 */}
+      <View style={styles.container}>
+        <Modal
+          visible={isExcludedOpen}
+          onShow={() => console.log('제외대상 모달창')}
+          onRequestClose={() => setIsExcludedOpen(false)}
+          transparent={true} // 필요에 따라 true로 설정할 수 있습니다.
+          style={{ alignItems: 'center' }}
+          >
+          <TouchableOpacity style={styles.modalBackground}  activeOpacity={1} onPressOut={() => setIsExcludedOpen(false)}>
+            <View style={styles.modalContainer}>
+              <Text style={styles.modalTitle}>제외대상 매칭</Text>
+              <Text style={styles.modalContent}>제외대상 매칭 내용</Text>
+              <TouchableOpacity style={styles.closeButton} onPress={() => setIsExcludedOpen(false)}>
+                <Text style={styles.closeButtonText}>X</Text>
+              </TouchableOpacity>
+            </View>
+          </TouchableOpacity>
+        </Modal>
+      </View>
     </P.Container>
   );
 };
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)', // 배경을 반투명하게 설정
+  },
+  modalBackground: {
+    flex: 1,
+    justifyContent: 'center', // 화면 상단에 위치시키기
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)', // 배경을 반투명하게 설정
+  },
+  modalContainer: {
+    width: '90%',
+    height: '50%',
+    backgroundColor: 'white',
+    borderRadius: 10,
+    padding: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  modalTitle: {
+    fontSize: 20,
+    marginBottom: 10,
+  },
+  modalContent: {
+    fontSize: 16,
+    marginBottom: 20,
+  },
+  closeButton: {
+    position: 'absolute',
+    top: 10,
+    right: 10,
+    padding: 10,
+  },
+  closeButtonText: {
+    fontSize: 18,
+    color: 'black',
+  },
+});
 
 export default PolicyDetail_fin;
